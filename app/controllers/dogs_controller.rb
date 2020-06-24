63aa48aa0d60cf296176e8cdb5dfa3b71897b425
arthur318 <arthurhwang318@gmail.com>
2020-06-24 11:59:52 -0700

    done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,16 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+        @employees = @dog.employees
+    end
+
+    def dog_params
+        params.require(:dog).permit!
+    end
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,53 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def new
+        @employee = Employee.new
+        @dogs = Dog.all
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+        @dogs = Dog.all
+    end
+
+    def create
+        @employee = Employee.new(employee_params)
+        if @employee.valid?
+            @employee.save
+            redirect_to employee_path(@employee)
+        else
+            render :new
+        end
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        @employee.update(employee_params)
+        if @employee.valid?
+            @employee.save
+            redirect_to "/employees/#{@employee.id}"
+        else
+            render :edit
+        end
+    end
+    
+    def destroy
+        @employee = Employee.find(params[:id])
+        @employee.destroy
+        redirect_to "/employees"
+    end
+
+
+    def employee_params
+        params.require(:employee).permit!
+    end
+
 end
